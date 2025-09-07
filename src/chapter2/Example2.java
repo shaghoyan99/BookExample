fa15400209e9fd684810b8229154cdad9f861002
Artash Shaghoyan <shagoyan99@gmail.com>
2025-09-07 20:44:03 +0400

    Add Code

---
+++ src/chapter2/Example2.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num = 100;
+
+        System.out.println("This is number: " + num);
+
+        num = num * 2;
+
+        System.out.print("Number * 2: " + num);
+
+    }
+}
