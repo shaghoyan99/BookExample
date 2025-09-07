d4f30071c97e7811cf1bbc4510ff5d9b29e76fdd
Artash Shaghoyan <shagoyan99@gmail.com>
2025-09-07 21:12:57 +0400

    Add Code

---
+++ src/chapter3/Area.java
@@ -0,0 +1,11 @@
+package chapter3;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi = 3.1416;
+        double r = 10.8;
+        double a = pi * r * r;
+
+        System.out.println("Плoщaдь круга равна " + a);
+    }
+}
---
+++ src/chapter3/CharDemo.java
@@ -0,0 +1,10 @@
+package chapter3;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1 = 88;
+        char ch2 = 'Y';
+
+        System.out.println("ch1 и ch2: " + ch1 + " - " + ch2);
+    }
+}
