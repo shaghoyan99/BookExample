0e4612998ee2dc7660d72090cb34f6bb44d87e6c
Artash Shaghoyan <shagoyan99@gmail.com>
2025-09-07 20:56:41 +0400

    Add Code

---
+++ src/chapter2/BlockTest.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class BlockTest {
+    public static void main(String[] args) {
+
+        int y = 20;
+
+        for (int x = 0; x < 10; x++) {
+            System.out.println("Знaчeниe х: " + x);
+            System.out.println("Знaчeниe y: " + y);
+            y = y - 2;
+        }
+    }
+}
