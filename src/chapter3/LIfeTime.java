0290afe4d1fe1d5d681bcd86f2c6a32104b088ac
Artash Shaghoyan <shagoyan99@gmail.com>
2025-09-08 11:08:33 +0400

    Add book example

---
+++ src/chapter3/LIfeTime.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class LIfeTime {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+            int y = -1;
+            System.out.println("у равно: " + y);
+            y = 100;
+            System.out.println("у теперь равно: " + y);
+        }
+    }
+}
